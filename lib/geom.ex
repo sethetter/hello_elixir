7a8e6aa1fc3e2a5fd324eb5e0f36ab925686369e
Seth Etter <sethetter@gmail.com>
2016-06-26 22:02:23 -0500

    3-4

--- lib/geom.ex
+++ lib/geom.ex
@@ -13,12 +13,14 @@ defmodule Geom do
   the product of it's arguments. Default values for each element are 1.
   """
 
-  @spec area(:atom, number(), number()) :: number()
+  @spec area({:atom, number(), number()}) :: number()
 
-  def area(:rectangle, w, h) when w >= 0 and h >= 0, do: w * h
-  def area(:triangle, w, h) when w >= 0 and h >= 0, do: w * h / 2.0
-  def area(:ellipse, r1, r2) when r1 >= 0 and r2 >= 0, do: :math.pi() * r1 * r2
-  def area(_, _, _), do: 0
+  def area({shape, x, y}), do: area(shape, x, y)
+
+  defp area(:rectangle, w, h) when w >= 0 and h >= 0, do: w * h
+  defp area(:triangle, w, h) when w >= 0 and h >= 0, do: w * h / 2.0
+  defp area(:ellipse, r1, r2) when r1 >= 0 and r2 >= 0, do: :math.pi() * r1 * r2
+  defp area(_, _, _), do: 0
 
 
 end
--- test/geom_test.exs
+++ test/geom_test.exs
@@ -3,28 +3,34 @@ defmodule GeomTest do
   doctest Geom
 
   test "multiplies areas for rectangles" do
-    assert Geom.area(:rectangle, 4, 5) == 20
-    assert Geom.area(:rectangle, 4, 4) == 16
+    assert Geom.area({:rectangle, 4, 5}) == 20
+    assert Geom.area({:rectangle, 4, 4}) == 16
   end
 
   test "multiplies areas for triangles" do
-    assert Geom.area(:triangle, 4, 5) == 10
-    assert Geom.area(:triangle, 4, 4) == 8
+    assert Geom.area({:triangle, 4, 5}) == 10
+    assert Geom.area({:triangle, 4, 4}) == 8
   end
 
   test "multiples areas for ellipsis" do
-    assert Geom.area(:ellipse, 5, 2) == 31.41592653589793
-    assert Geom.area(:ellipse, 3, 7) == 65.97344572538566
+    assert Geom.area({:ellipse, 5, 2}) == 31.41592653589793
+    assert Geom.area({:ellipse, 3, 7}) == 65.97344572538566
   end
 
   test "responds to invalid parameters with 0" do
-    assert Geom.area(:rectangle, -1, 5) == 0
-    assert Geom.area(:rectangle, 1, -5) == 0
-    assert Geom.area(:triangle, -1, 5) == 0
-    assert Geom.area(:triangle, 1, -5) == 0
-    assert Geom.area(:ellipse, -1, 5) == 0
-    assert Geom.area(:ellipse, 1, -5) == 0
-    assert Geom.area(:lolwat, 1, 2) == 0
+    assert Geom.area({:rectangle, -1, 5}) == 0
+    assert Geom.area({:rectangle, 1, -5}) == 0
+    assert Geom.area({:triangle, -1, 5}) == 0
+    assert Geom.area({:triangle, 1, -5}) == 0
+    assert Geom.area({:ellipse, -1, 5}) == 0
+    assert Geom.area({:ellipse, 1, -5}) == 0
+    assert Geom.area({:lolwat, 1, 2}) == 0
+  end
+
+  test "can not call private area function directly" do
+    assert_raise UndefinedFunctionError, fn() ->
+      Geom.area(:rectangle, 1, 2)
+    end
   end
 
 end
