43ff1f4b7e45070fb136790f61eb9b7e1792a4f9
Seth Etter <sethetter@gmail.com>
2016-06-26 22:09:17 -0500

    4-1

--- lib/geom.ex
+++ lib/geom.ex
@@ -13,14 +13,15 @@ defmodule Geom do
   the product of it's arguments. Default values for each element are 1.
   """
 
-  @spec area({:atom, number(), number()}) :: number()
+  @spec area(:atom, number(), number()) :: number()
 
-  def area({shape, x, y}), do: area(shape, x, y)
-
-  defp area(:rectangle, w, h) when w >= 0 and h >= 0, do: w * h
-  defp area(:triangle, w, h) when w >= 0 and h >= 0, do: w * h / 2.0
-  defp area(:ellipse, r1, r2) when r1 >= 0 and r2 >= 0, do: :math.pi() * r1 * r2
-  defp area(_, _, _), do: 0
-
+  def area(shape, x, y) when x >= 0 and y >= 0 do
+    case shape do
+      :rectangle -> x * y
+      :triangle -> x * y / 2.0
+      :ellipse -> :math.pi() * x * y
+      _ -> 0
+    end
+  end
 
 end
