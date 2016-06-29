520e6df9857031fba057c61e997570507c19a986
Seth Etter <sethetter@gmail.com>
2016-06-28 22:13:08 -0500

    4-4

--- lib/powers.ex
+++ lib/powers.ex
@@ -1,13 +1,14 @@
 defmodule Powers do
-  import Kernel, except: [raise: 2]
+  import Kernel, except: [raise: 2, raise: 3]
 
   @doc"""
   Raises the first number to the power of the second number.
   """
   @spec raise(number(), number()) :: number()
 
-  def raise(base, power) when power == 0, do: 1
-  def raise(base, power) when power == 1, do: base
-  def raise(base, power) when power > 0, do: base * raise(base, power - 1)
+  def raise(_, power) when power == 0, do: 1
   def raise(base, power) when power < 0, do: 1.0 / raise(base, power * -1)
+  def raise(base, power) when power > 0, do: raise(base, power, 1)
+  def raise(_, 0, acc), do: acc
+  def raise(base, power, acc), do: raise(base, power - 1, base * acc)
 end
